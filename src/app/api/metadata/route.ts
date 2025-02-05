23a5c23719d4e8061b1b86a7e7aa1d685a2e5bee
MatiNav <matiasnavarroezequiel@gmail.com>
2025-02-05 20:40:49 -0300

    make routes dynamic to fix deploy

--- src/app/api/metadata/route.ts
+++ src/app/api/metadata/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 import { DBPlayer } from "@/app/constants/types";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   try {
     // Verify secret from Authorization header
