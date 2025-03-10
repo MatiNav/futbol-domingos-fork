bd9631b70bf9f5ddc45ff6561e62e5b8821d8420
MatiNav <matiasnavarroezequiel@gmail.com>
2025-03-10 16:35:16 -0300

    random changes to check run

--- infrastructure/cloud_build_trigger.tf
+++ infrastructure/cloud_build_trigger.tf
@@ -10,6 +10,28 @@ resource "google_cloudbuild_trigger" "nextjs_cloud_run_trigger" {
     }
   }
 
+  substitutions = {
+    _SERVICE_NAME = "futbol-domingos-nextjs-cloud-run-service"
+    _PROJECT_ID = var.project_id
+    _REGION = var.region
+    _SERVICE_ACCOUNT = var.service_account
+    _GCP_FUTBOL_APP_BUCKET_NAME = var.GCP_FUTBOL_APP_BUCKET_NAME  
+    _MONGODB_URI = var.MONGODB_URI
+    _REDIS_HOST = var.REDIS_HOST
+    _REDIS_PORT = var.REDIS_PORT
+    _REDIS_PASSWORD = var.REDIS_PASSWORD
+    _REDIS_USERNAME = var.REDIS_USERNAME
+    _PUSHER_APP_ID = var.PUSHER_APP_ID
+    _PUSHER_KEY = var.PUSHER_KEY
+    _PUSHER_SECRET = var.PUSHER_SECRET
+    _PUSHER_CLUSTER = var.PUSHER_CLUSTER
+    _NEXT_PUBLIC_PUSHER_KEY = var.NEXT_PUBLIC_PUSHER_KEY
+    _NEXT_PUBLIC_PUSHER_CLUSTER = var.NEXT_PUBLIC_PUSHER_CLUSTER
+    _GCP_PROJECT_ID = var.GCP_PROJECT_ID
+    _GCP_CLIENT_EMAIL = var.GCP_CLIENT_EMAIL
+    _GCP_PRIVATE_KEY = var.GCP_PRIVATE_KEY
+  }
+
   filename = "infrastructure/cloud_build/cloudbuild.yaml"  # Cloud Build configuration file path in repo
 
   included_files = ["infrastructure/cloud_build/cloudbuild.yaml", "infrastructure/cloud_build/Dockerfile", "**/*.js", "**/*.ts", "**/*.tsx", "**/*.json"]
--- infrastructure/cloud_run.tf
+++ infrastructure/cloud_run.tf
@@ -1,45 +1,45 @@
 
-variable "cloud_run_service_name" {
-  type = string
-  description = "The name of the Cloud Run service"
-  default = "futbol-domingos-nextjs-cloud-run-service"
-}
+# variable "cloud_run_service_name" {
+#   type = string
+#   description = "The name of the Cloud Run service"
+#   default = "futbol-domingos-nextjs-cloud-run-service"
+# }
 
-resource "google_cloud_run_service" "futbol-domingos-nextjs-cloud-run-service" {
-  name     = var.cloud_run_service_name
-  location = var.region
+# resource "google_cloud_run_service" "futbol-domingos-\nextjs-cloud-run-service" {
+#   name     = var.cloud_run_service_name
+#   location = var.region
   
-  template {
-    spec {
-      containers {
-        image = "us-central1-docker.pkg.dev/${var.project_id}/cloud-run-source-deploy/${var.cloud_run_service_name}:latest"
+#   template {
+#     spec {
+#       containers {
+#         image = "us-central1-docker.pkg.dev/${var.project_id}/cloud-run-source-deploy/${var.cloud_run_service_name}:latest"
         
-        # Lower resource limits to stay within free tier
-        resources {
-          limits = {
-            cpu    = "1"
-            memory = "512Mi"  # Minimal memory to stay in free tier
-          }
-        }
-      }
-      # Scale to zero when not in use to minimize costs
-      container_concurrency = 80
-    }
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale" = "5"  # Lower max instances
-        "autoscaling.knative.dev/minScale" = "0"  # Scale to zero when not in use
-        "run.googleapis.com/cpu-throttling" = "true"  # Request-based billing
-        "run.googleapis.com/startup-cpu-boost" = "true"  # Faster cold starts
-      }
-    }
-  }
-}
+#         # Lower resource limits to stay within free tier
+#         resources {
+#           limits = {
+#             cpu    = "1"
+#             memory = "512Mi"  # Minimal memory to stay in free tier
+#           }
+#         }
+#       }
+#       # Scale to zero when not in use to minimize costs
+#       container_concurrency = 80
+#     }
+#     metadata {
+#       annotations = {
+#         "autoscaling.knative.dev/maxScale" = "5"  # Lower max instances
+#         "autoscaling.knative.dev/minScale" = "0"  # Scale to zero when not in use
+#         "run.googleapis.com/cpu-throttling" = "true"  # Request-based billing
+#         "run.googleapis.com/startup-cpu-boost" = "true"  # Faster cold starts
+#       }
+#     }
+#   }
+# }
 
-# IAM policy to make the service publicly accessible
-resource "google_cloud_run_service_iam_member" "public_access" {
-  service  = google_cloud_run_service.futbol-domingos-nextjs-cloud-run-service.name
-  location = google_cloud_run_service.futbol-domingos-nextjs-cloud-run-service.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
+# # IAM policy to make the service publicly accessible
+# resource "google_cloud_run_service_iam_member" "public_access" {
+#   service  = google_cloud_run_service.futbol-domingos-nextjs-cloud-run-service.name
+#   location = google_cloud_run_service.futbol-domingos-nextjs-cloud-run-service.location
+#   role     = "roles/run.invoker"
+#   member   = "allUsers"
+# }
